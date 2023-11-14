f42b81a94fb2323c3b74485787b11eb37e28580d
qiush <21574127@qq.com>
2023-11-14 21:02:45 +0800

    插件固定版本

---
+++ src/main/java/com/bonc/springboot/config/Father.java
@@ -0,0 +1,11 @@
+package com.bonc.springboot.config;
+
+/**
+ * @author qiush
+ * @create 2023-07-31 10:39
+ */
+public class Father extends GFanther{
+    public Father( ) {
+        System.out.println("父类构造");
+    }
+}
